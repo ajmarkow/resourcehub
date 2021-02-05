74ec8d0e8be72fe7827daf070e6d23591f450358
AJ Markow <alexanderjmarkow@gmail.com>
2021-02-05 14:44:22 -0800

    Implement get all endpoint.
    Co-authored-by: Chloe Hellberg <chloe.hellberg@gmail.com>
    Co-authored-by: Ben White <bwhite2140@outlook.com>

--- randomget.js
+++ randomget.js
@@ -2,25 +2,18 @@ import handler from "./libs/handler-lib";
 import dynamoDb from "./libs/dynamodb-lib";
 
 export const main = handler(async (event, context) => {
-
-
   const params = {
-    TableName: process.env.tableName,
-    ExpressionAttributeValues: {
-      ":high": 5
-    },
-    FilterExpression: 'postRating = :high',
-    ProjectionExpression: "userId, postId",
+    TableName: process.env.tableName
   };
-
 
   const result = await dynamoDb.scan(params);
   const returnedPostsCount = result.Count;
+
   console.log(returnedPostsCount);
-    console.log(result);
+  console.log(result);
   if (!result) {
     throw new Error("Items not located!");
   }
 
-  return result.Item;
-  });
+  return result;
+});
